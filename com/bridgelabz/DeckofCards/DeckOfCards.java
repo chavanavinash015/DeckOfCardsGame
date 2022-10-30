9f076924ae854f7167b82596ce0bfdaecea232fa
arc-015 <chavanavinash015@gmail.com>
2022-10-30 15:14:21 +0530

    Welcome to the deck of cards game

---
+++ com/bridgelabz/DeckofCards/DeckOfCards.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.DeckofCards;
+
+public class DeckOfCards {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Deck of Cards Game ");
+        
+    }
+}
